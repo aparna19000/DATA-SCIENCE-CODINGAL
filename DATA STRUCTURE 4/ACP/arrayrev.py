ed11e464e8d8504d5ce934d851e26833436c9d85
aparna19000 <54104999+aparna19000@users.noreply.github.com>
2026-08-29 13:44:25 +0530

    Add files via upload

---
+++ DATA STRUCTURE 4/ACP/arrayrev.py
@@ -0,0 +1,16 @@
+original_array = [6, 5, 4, 3, 2, 1]
+
+
+reversed_array = original_array[::-1]
+
+print("Original array :", original_array)
+print("Reversed array :", reversed_array)
+
+
+#2nd way
+
+original_array = [6, 5, 4, 3, 2, 1]
+
+original_array.reverse()
+
+print("Reversed array :", original_array)
